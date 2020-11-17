1343c8fa1a00539a746eacc34a52c7abb8b9ba2e
NataliaSilantieva <63507488+NataliaSilantieva@users.noreply.github.com>
2020-11-17 21:43:19 +0200

    Create task_1.js

---
+++ task_1.js
@@ -0,0 +1,19 @@
+const delay = ms => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            
+            resolve(ms);
+            reject ("error")
+        },
+             ms);
+    });
+};
+  
+  
+  const logger = time => console.log(`Resolved after ${time}ms`);
+ 
+  
+  // Вызовы функции для проверки
+  delay(2000).then(logger); 
+  delay(1000).then(logger); 
+  delay(1500).then(logger); 
